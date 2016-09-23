6c1b6f999c3300d778b03bff9216f8d48ecdb291
lafa5 <lafa510@gmail.com>
2016-09-24 01:39:54 +0900

    add mainhomeactivity
    mainhome & pageradapter & card image

--- MAIN/app/src/main/java/com/bluehack/nbbang/MainHomeActivity.java
+++ MAIN/app/src/main/java/com/bluehack/nbbang/MainHomeActivity.java
@@ -1,13 +1,23 @@
 package com.bluehack.nbbang;
 
+import android.graphics.pdf.PdfDocument;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainHomeActivity extends AppCompatActivity {
 
+    ViewPager mViewPager;
+    PagerAdapter mPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
+
+        mViewPager = (ViewPager) findViewById(R.id.main_card_viewpager);
+        mPagerAdapter = new MainViewPagerAdapter(getApplicationContext(), 3);
+        mViewPager.setAdapter(mPagerAdapter);
     }
 }
---
+++ MAIN/app/src/main/java/com/bluehack/nbbang/MainViewPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.bluehack.nbbang;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by jayman on 2016-09-24.
+ */
+
+public class MainViewPagerAdapter extends PagerAdapter {
+
+    Context mContext;
+    LayoutInflater mInflater;
+    int mCardNum = 0;
+
+    public MainViewPagerAdapter(Context mContext, int mCardNum) {
+        this.mContext = mContext;
+        this.mCardNum = mCardNum;
+        mInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public int getCount() {
+        if(mCardNum < 0) return 0;
+        return mCardNum;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        View view = mInflater.inflate(R.layout.view_pager_child_view, null);
+
+        ImageView imgView = (ImageView)view.findViewById(R.id.viewpager_child_img);
+
+        imgView.setImageResource(R.drawable.master_home_card);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View)object);
+    }
+
+    public void setmCardNum(int mCardNum) {
+        this.mCardNum = mCardNum;
+    }
+}
