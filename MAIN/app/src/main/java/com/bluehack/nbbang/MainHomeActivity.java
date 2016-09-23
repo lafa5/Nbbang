a232ae56ab6424ccd6cce00c7dbf654142b0288c
lafa5 <lafa510@gmail.com>
2016-09-23 22:48:07 +0900

    start
    start

---
+++ MAIN/app/src/main/java/com/bluehack/nbbang/MainHomeActivity.java
@@ -0,0 +1,13 @@
+package com.bluehack.nbbang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home);
+    }
+}
