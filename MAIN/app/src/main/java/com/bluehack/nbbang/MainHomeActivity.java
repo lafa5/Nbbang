7be0d97ff0801ffe417829ed393a6734881b79fe
lafa5 <lafa510@gmail.com>
2016-09-24 03:15:44 +0900

    add fingerprint logic

--- MAIN/app/src/main/java/com/bluehack/nbbang/MainHomeActivity.java
+++ MAIN/app/src/main/java/com/bluehack/nbbang/MainHomeActivity.java
@@ -1,23 +1,37 @@
 package com.bluehack.nbbang;
 
+import android.content.Context;
 import android.graphics.pdf.PdfDocument;
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.samsung.android.sdk.pass.Spass;
+import com.samsung.android.sdk.pass.SpassFingerprint;
 
 public class MainHomeActivity extends AppCompatActivity {
 
+    private static String TAG = "MainHomeActivity";
+
+    private Context mContext;
+
     private ViewPager mViewPager;
     private PagerAdapter mPagerAdapter;
+    private Spass mSpass;
+    private SpassFingerprint mFingerPrint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
 
+        mContext = getApplicationContext();
+
         mViewPager = (ViewPager) findViewById(R.id.main_card_viewpager);
-        mPagerAdapter = new MainViewPagerAdapter(getApplicationContext(), 3);
+        mPagerAdapter = new MainViewPagerAdapter(mContext, 3);
         mViewPager.setAdapter(mPagerAdapter);
     }
 }
--- MAIN/app/src/main/java/com/bluehack/nbbang/MainViewPagerAdapter.java
+++ MAIN/app/src/main/java/com/bluehack/nbbang/MainViewPagerAdapter.java
@@ -2,10 +2,15 @@ package com.bluehack.nbbang;
 
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import com.samsung.android.sdk.SsdkUnsupportedException;
+import com.samsung.android.sdk.pass.Spass;
+import com.samsung.android.sdk.pass.SpassFingerprint;
 
 /**
  * Created by jayman on 2016-09-24.
@@ -13,16 +18,60 @@ import android.widget.ImageView;
 
 public class MainViewPagerAdapter extends PagerAdapter {
 
+    private static String TAG = "MainViewPagerAdapter";
+
     private Integer[] cardImageArr = {R.drawable.master_home_card, R.drawable.master_home_card, R.drawable.master_home_card};
 
     private Context mContext;
     private LayoutInflater mInflater;
     private int mCardNum = 0;
 
+    private Spass mSpass;
+    private SpassFingerprint mFingerPrint;
+
+    private SpassFingerprint.IdentifyListener mFingerPrintListener;
+
     public MainViewPagerAdapter(Context mContext, int mCardNum) {
         this.mContext = mContext;
         this.mCardNum = mCardNum;
         mInflater = LayoutInflater.from(mContext);
+
+        mSpass = new Spass();
+
+        try{
+            mSpass.initialize(mContext);
+        } catch (SsdkUnsupportedException e){
+            Log.e(TAG, e.toString());
+        }catch (UnsupportedOperationException e){
+            Log.e(TAG, e.toString());
+        }
+
+        if(mSpass.isFeatureEnabled(Spass.DEVICE_FINGERPRINT)){
+            mFingerPrint = new SpassFingerprint(mContext);
+            mFingerPrintListener = new SpassFingerprint.IdentifyListener() {
+                @Override
+                public void onFinished(int i) {
+
+                }
+
+                @Override
+                public void onReady() {
+
+                }
+
+                @Override
+                public void onStarted() {
+
+                }
+
+                @Override
+                public void onCompleted() {
+
+                }
+            };
+        } else {
+            Log.d(TAG, "Fingerprint Service is not supported in the device.");
+        }
     }
 
     @Override
@@ -46,6 +95,13 @@ public class MainViewPagerAdapter extends PagerAdapter {
         imgView.setImageResource(cardImageArr[position]);
 
         container.addView(view);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFingerPrint.startIdentifyWithDialog(mContext, mFingerPrintListener, true);
+            }
+        });
 
         return view;
     }
